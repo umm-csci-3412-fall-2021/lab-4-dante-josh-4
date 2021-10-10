f8aa606084b887c7e8b86ceb1e6fd0b89ac4ad28
Dante Miller <mill8622@rynite.morris.umn.edu>
2021-10-09 20:29:23 -0500

    Work on implementation of summarize_tree.c
    This commit works on the implementation of summarize_tree.c
    
    Co-authored-by: Joshua Eklund <eklun124@umn.edu>

--- summarize_tree/summarize_tree.c
+++ summarize_tree/summarize_tree.c
@@ -9,33 +9,35 @@
 static int num_dirs, num_regular;
 
 bool is_dir(const char* path) {
-  /*
-   * Use the stat() function (try "man 2 stat") to determine if the file
-   * referenced by path is a directory or not.  Call stat, and then use
-   * S_ISDIR to see if the file is a directory. Make sure you check the
-   * return value from stat() in case there is a problem, e.g., maybe the
-   * the file doesn't actually exist.
-   */
+	struct stat buffer;
+	if(stat(path,&buffer)==0){
+		if(S_ISDIR(buf.st_mode) != 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	else{
+		return false;
+	}
 }
-
-/* 
- * I needed this because the multiple recursion means there's no way to
- * order them so that the definitions all precede the cause.
- */
 void process_path(const char*);
 
+
 void process_directory(const char* path) {
-  /*
-   * Update the number of directories seen, use opendir() to open the
-   * directory, and then use readdir() to loop through the entries
-   * and process them. You have to be careful not to process the
-   * "." and ".." directory entries, or you'll end up spinning in
-   * (infinite) loops. Also make sure you closedir() when you're done.
-   *
-   * You'll also want to use chdir() to move into this new directory,
-   * with a matching call to chdir() to move back out of it when you're
-   * done.
-   */
+	DIR *folder;
+	struct dirent *dir;
+	folder = opendir(path);
+	if(folder){
+		while((dir = readdir(folder)) != NULL){
+			chdir(dir);
+			process_path(dir);
+
+		}
+		closedir(folder);
+	}
+
 }
 
 void process_file(const char* path) {
