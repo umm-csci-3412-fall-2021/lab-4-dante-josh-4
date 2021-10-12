e55fca09b1ad67e54c4e45c28fb97fbe7f1d6cb6
Joshua Eklund <eklun124@rynite.morris.umn.edu>
2021-10-12 10:38:27 -0500

    Add comments to disemvowel
    This commit adds comments to disemvowel for clarity.
    
    Co-authored-by: Dante Miller <mill8622@morris.umn.edu>

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -4,6 +4,7 @@
 
 #define BUF_SIZE 1024
 
+// check if current character is a vowel
 bool is_vowel(char ch) {
     if (ch =='a' || ch =='e' || ch =='i' || ch == 'o' || ch == 'u')
 		return true;
@@ -13,8 +14,11 @@ bool is_vowel(char ch) {
 	 return false;
 }
 
+
 int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
     int counter = 0;
+
+    // copy the non-vowel characters from in buffer to out buffer
     for(int i = 0; i < num_chars; i++){
     	if(is_vowel(in_buf[i]) == false){
 		out_buf[counter] = in_buf[i];
@@ -29,9 +33,14 @@ void disemvowel(FILE* inputFile, FILE* outputFile) {
     char* outputBuffer = (char*) calloc(BUF_SIZE,sizeof(char));
     int count_nonvowels;
     int chunk_size = 1;
+
+    // while there is still info to read
     while (chunk_size != 0) {
+	 // read file up to buf size and store info in chunk_size
 	 chunk_size = fread(inputBuffer,sizeof(char),BUF_SIZE,inputFile);
+	 // call copy_non_vowels on chunk_size and store result in count_nonvowels
 	 count_nonvowels = copy_non_vowels(chunk_size,inputBuffer,outputBuffer);
+	 // write current sequence of  nonvowel characters in outputBuffer to outputFile
  	 fwrite(outputBuffer,sizeof(char),count_nonvowels,outputFile);
 	}
     free(inputBuffer);
@@ -40,17 +49,28 @@ void disemvowel(FILE* inputFile, FILE* outputFile) {
 int main(int argc, char *argv[]) {
 	FILE *inputFile;
         FILE *outputFile;
+
+	// if no additional arguments are called
+	// set inputFile and outputFile to standard input and output
 	if(argc == 1){
 		inputFile = stdin;
 		outputFile = stdout;
 		disemvowel(inputFile, outputFile);
 	}
+
+	// if script called with one additional argument
+	// set input file to argument given
+	// set outputFile to standard output
 	if(argc == 2){
 		inputFile = fopen(argv[1],"r+");
 		outputFile = stdout;
 		disemvowel(inputFile, outputFile);
 		fclose(inputFile);
 	}
+
+	// if script was called with two additional arguments
+	// set inputFile to first argument given
+	// set outputFile to second argument given
 	if(argc == 3){
 		inputFile = fopen(argv[1],"r+");
 		outputFile = fopen(argv[2], "w+");
